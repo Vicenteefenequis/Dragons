fde261d5017e0ee3b984ebeb69e0b61fd56bf19a
Vicente Junior <vicente19981@live.com>
2022-01-13 19:58:10 -0300

    feat: criado page Register

---
+++ src/pages/Register/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { DefaultLayout } from '../../components';
+
+const Register = (): JSX.Element => {
+  return (
+    <DefaultLayout>
+      <p>ola register</p>
+    </DefaultLayout>
+  );
+};
+
+export default Register;
