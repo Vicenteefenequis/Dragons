3f6bd89606fe5e0ccd7baef89f65115d4fd0afca
Vicente Junior <vicente19981@live.com>
2022-01-13 19:59:14 -0300

    feat: create header component comportalihado

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { FaDragon } from 'react-icons/fa';
+import { ImPlus } from 'react-icons/im';
+import { NavLink } from 'react-router-dom';
+import './styles.scss';
+
+const Header = (): JSX.Element => {
+  return (
+    <div className="header">
+      <NavLink
+        to="/"
+        className={({ isActive }) =>
+          isActive ? 'menu__content active' : 'menu__content'
+        }
+      >
+        <FaDragon size={30} />
+        <h1>Home</h1>
+      </NavLink>
+      <NavLink
+        to="/register"
+        className={({ isActive }) =>
+          isActive ? 'menu__content active' : 'menu__content'
+        }
+      >
+        <ImPlus size={30} />
+        <h1>Cadastrar</h1>
+      </NavLink>
+    </div>
+  );
+};
+
+export default Header;
