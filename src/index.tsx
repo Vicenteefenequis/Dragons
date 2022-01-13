baa8327792124c4f64abc2f186e804c19119a861
Vicente Junior <vicente19981@live.com>
2022-01-13 17:40:04 -0300

    refactor: trocando app para router

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Router from './routes/Router';
 import './styles/global.scss';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router />
   </React.StrictMode>,
   document.getElementById('root'),
 );
