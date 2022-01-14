004a594f21365b0a597133c1898eb4a18b270093
Vicente Junior <vicente19981@live.com>
2022-01-14 11:23:37 -0300

    feat: usando dragons seletores redux e dispatch  action get dragons

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,53 +1,34 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Card, DefaultLayout } from '../../components';
+import { GetDragons } from '../../store/modules/dragons';
+import { RootState } from '../../store/modules/rootTypes';
 import './styles.scss';
 
 const Home = (): JSX.Element => {
+  const { dragons } = useSelector((state: RootState) => ({
+    dragons: state.dragon.dragons,
+  }));
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(GetDragons());
+  }, []);
+
   return (
     <DefaultLayout>
       <div className="home">
-        <Card
-          position={1}
-          name="Cratos Dragon"
-          canDelete={() => console.log('eerae')}
-          canEdit={() => console.log('edit')}
-        />
-        <Card
-          position={1}
-          name="Cratos Dragon"
-          canDelete={() => console.log('eerae')}
-          canEdit={() => console.log('edit')}
-        />
-        <Card
-          position={1}
-          name="Cratos Dragon"
-          canDelete={() => console.log('eerae')}
-          canEdit={() => console.log('edit')}
-        />
-        <Card
-          position={1}
-          name="Cratos Dragon"
-          canDelete={() => console.log('eerae')}
-          canEdit={() => console.log('edit')}
-        />
-        <Card
-          position={1}
-          name="Cratos Dragon"
-          canDelete={() => console.log('eerae')}
-          canEdit={() => console.log('edit')}
-        />
-        <Card
-          position={1}
-          name="Cratos Dragon"
-          canDelete={() => console.log('eerae')}
-          canEdit={() => console.log('edit')}
-        />
-        <Card
-          position={1}
-          name="Cratos Dragon"
-          canDelete={() => console.log('eerae')}
-          canEdit={() => console.log('edit')}
-        />
+        {dragons.map((dragon, key) => (
+          <button key={key}>
+            <Card
+              position={dragon.id}
+              name={dragon.name}
+              canDelete={() => console.log('eerae')}
+              canEdit={() => console.log('edit')}
+            />
+          </button>
+        ))}
       </div>
     </DefaultLayout>
   );
