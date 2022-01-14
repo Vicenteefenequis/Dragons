6b386161edadad24418caf0c899071f841163066
Vicente Junior <vicente19981@live.com>
2022-01-14 11:22:05 -0300

    feat: aplicando middleware do saga no redux

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,14 @@
-import { createStore } from 'redux';
+import createSagaMiddleware from '@redux-saga/core';
+import { applyMiddleware, createStore } from 'redux';
 import rootReducer from './modules/rootReducer';
+import rootSaga from './modules/rootSaga';
 import persistReducers from './persistReducers';
 
 const persistedReducer = persistReducers(rootReducer);
+const sagaMiddleware = createSagaMiddleware();
 
-const store = createStore(persistedReducer);
+const store = createStore(persistedReducer, applyMiddleware(sagaMiddleware));
+
+sagaMiddleware.run(rootSaga);
 
 export { store };
