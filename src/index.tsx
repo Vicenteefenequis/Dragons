1f7abc1bf79e9464883656e48a8ab2f53033b993
Vicente Junior <vicente19981@live.com>
2022-01-13 22:20:43 -0300

    feat: passando o provider do reducer ao redor das rotas

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import Router from './routes/Router';
+import { store } from './store';
 import './styles/global.scss';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router />
+    <Provider store={store}>
+      <Router />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
