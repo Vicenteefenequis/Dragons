1f0bc89068b1d91335af55ad5b665097abeb62d7
Vicente Junior <vicente19981@live.com>
2022-01-14 17:41:59 -0300

    feat: criando pagina de detalhes de dragoes

---
+++ src/pages/Detail/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { useParams } from 'react-router';
+import { CardDetail, DefaultLayout } from '../../components';
+import { RootState } from '../../store/modules/rootTypes';
+import './styles.scss';
+
+const Detail = (): JSX.Element => {
+  const { dragonId } = useParams();
+
+  const { dragon } = useSelector((state: RootState) => ({
+    dragon: state.dragon.dragons.find(dragon => dragon.id === dragonId),
+  }));
+
+  return (
+    <DefaultLayout>
+      <CardDetail
+        name={dragon?.name || ''}
+        type={dragon?.type || ''}
+        createdAt={dragon?.createdAt || ''}
+      />
+    </DefaultLayout>
+  );
+};
+
+export default Detail;
