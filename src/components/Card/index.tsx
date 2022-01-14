9ac5648126f4607debfe7605246363ce7cceb1f4
Vicente Junior <vicente19981@live.com>
2022-01-13 23:00:59 -0300

    feat: criado component de card para informacoes de dragoes

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { FaTrash, FaPen } from 'react-icons/fa';
+import './styles.scss';
+
+type Props = {
+  position: number;
+  name: string;
+  canEdit: () => void;
+  canDelete: () => void;
+};
+
+const Card = ({ position, name, canDelete, canEdit }: Props): JSX.Element => {
+  return (
+    <div className="card">
+      <div className="wrapper__icons">
+        <button onClick={canDelete}>
+          <FaTrash size={20} className="trash" />
+        </button>
+        <button onClick={canEdit}>
+          <FaPen size={20} className="pen" />
+        </button>
+      </div>
+      <div className="main__content">
+        <h2>Nº {position}</h2>
+        <h3>{name}</h3>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
