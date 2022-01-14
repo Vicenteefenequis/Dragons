fe909bd4d25329648f0e23e959a27262cbdb25e9
Vicente Junior <vicente19981@live.com>
2022-01-14 17:43:13 -0300

    feat: export component card detail

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as DefaultLayout } from './DefaultLayout';
 export { default as Header } from './Header';
 export { default as Card } from './Card';
+export { default as CardDetail } from './CardDetail';
