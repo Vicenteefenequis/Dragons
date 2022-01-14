593c5e0e431aa946376c82cb7b2129e1fdfcea29
Vicente Junior <vicente19981@live.com>
2022-01-14 11:21:47 -0300

    feat: importando dragons para combine reducers

--- src/store/modules/rootReducer.ts
+++ src/store/modules/rootReducer.ts
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import { appReducer as app } from './app';
+import { dragonsReducer as dragon } from './dragons';
 
 export default combineReducers({
   app,
+  dragon,
 });
