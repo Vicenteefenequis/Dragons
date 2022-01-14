ecb0b86f39db30baa6ed56a431143122cfeb022b
Vicente Junior <vicente19981@live.com>
2022-01-14 20:59:55 -0300

    feat: criado component de button

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import LoadingSpinner from '../LoadingSpinner';
+import './styles.scss';
+
+type Props = {
+  color?: 'primary';
+  variant?: 'contained';
+  loading?: boolean;
+  children?: React.ReactNode;
+} & React.ButtonHTMLAttributes<HTMLButtonElement>;
+
+const Button = ({
+  color = 'primary',
+  variant = 'contained',
+  className,
+  children,
+  loading,
+  ...rest
+}: Props): JSX.Element => {
+  return (
+    <button className={[`${variant}-${color}-button`, className].join(' ')}>
+      {loading && <LoadingSpinner size={20} />}
+      {children}
+    </button>
+  );
+};
+
+export default Button;
