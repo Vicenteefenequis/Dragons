259d9b100c9b4d45a789f8c3c1c84228c3583e91
Vicente Junior <vicente19981@live.com>
2022-01-15 13:43:46 -0300

    feat: criado function para atualizar dragoes

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,31 +1,72 @@
-import React, { useEffect, useState } from 'react';
+/* eslint-disable @typescript-eslint/consistent-type-assertions */
+import React, { useCallback, useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router';
 import {
   Card,
   DefaultLayout,
-  InfoModal,
+  ModalContainer,
   LoadingSpinner,
+  Input,
 } from '../../components';
-import { DeleteDragon, GetDragons } from '../../store/modules/dragons';
+import { FormHandler } from '../../hooks/useForm';
+import {
+  DeleteDragon,
+  Dragon,
+  GetDragons,
+  PutDragon,
+} from '../../store/modules/dragons';
 import { RootState } from '../../store/modules/rootTypes';
 import useModal from '../../utils/useModal';
 import './styles.scss';
 
+type FormData = {
+  name: string;
+  type: string;
+};
+
 const Home = (): JSX.Element => {
-  const { isOpen, onClose, onOpen } = useModal();
+  const {
+    isOpen: isOpenDelete,
+    onClose: onCloseDelete,
+    onOpen: onOpenDelete,
+  } = useModal();
+  const {
+    isOpen: isOpenEdit,
+    onClose: onCloseEdit,
+    onOpen: onOpenEdit,
+  } = useModal();
   const navigate = useNavigate();
   const { dragons, status } = useSelector((state: RootState) => ({
     dragons: state.dragon.dragons,
     status: state.app.requestStatus,
   }));
   const [idDeleted, setIdDeleted] = useState('');
+  const [dragonEdit, setDragonEdit] = useState<Dragon>({} as Dragon);
 
   const dispatch = useDispatch();
 
   useEffect(() => {
     if (!dragons.length) dispatch(GetDragons());
   }, [dragons]);
+
+  const handleEdit = useCallback(
+    (data: FormData) => {
+      let { name, type } = data;
+
+      if (!type) {
+        type = dragonEdit.type;
+      }
+
+      if (!name) {
+        name = dragonEdit.name;
+      }
+
+      dispatch(PutDragon({ ...dragonEdit, name, type }));
+      onCloseEdit();
+    },
+    [dragonEdit],
+  );
 
   return (
     <DefaultLayout>
@@ -37,10 +78,13 @@ const Home = (): JSX.Element => {
               position={dragon.id}
               name={dragon.name}
               canDelete={() => {
-                onOpen();
+                onOpenDelete();
                 setIdDeleted(dragon.id);
               }}
-              canEdit={() => console.log('edit')}
+              canEdit={() => {
+                onOpenEdit();
+                setDragonEdit(dragon);
+              }}
               onClick={() => navigate(`/home/${dragon.id}`)}
             />
           ))
@@ -49,19 +93,46 @@ const Home = (): JSX.Element => {
             <LoadingSpinner size={200} />
           </div>
         )}
-        {isOpen && (
-          <InfoModal
-            title="Tem certeza que deseja excluir?"
-            description="Se voce excluir ira desaparecer um dragao!"
-            cancelText={'Cancel'}
-            approveText={'Continue'}
-            onClose={onClose}
-            clickApprove={() => {
-              onClose();
+        <ModalContainer
+          title={'Excluir Dragon'}
+          open={isOpenDelete}
+          close={onCloseDelete}
+        >
+          <p>Tem certeza que deseja excluir esse Dragon?</p>
+          <button
+            className="confirm__buton"
+            onClick={() => {
               dispatch(DeleteDragon({ id: idDeleted }));
+              onCloseDelete();
             }}
-          />
-        )}
+            type="submit"
+          >
+            Remover
+          </button>
+        </ModalContainer>
+        <ModalContainer
+          title={'Edit Dragon'}
+          open={isOpenEdit}
+          close={onCloseEdit}
+        >
+          <FormHandler onSubmit={data => handleEdit(data as FormData)}>
+            <Input
+              name="name"
+              defaultValue={dragonEdit.name}
+              placeholder={'Name'}
+              style={{ width: 200 }}
+            />
+            <Input
+              name="type"
+              defaultValue={dragonEdit.type}
+              placeholder={'Type'}
+              style={{ width: 200 }}
+            />
+            <button className="confirm__buton" type="submit">
+              Editar
+            </button>
+          </FormHandler>
+        </ModalContainer>
       </div>
     </DefaultLayout>
   );
