da3da900e5d93edc995b1c7d4428044c6f21d54d
Vicente Junior <vicente19981@live.com>
2022-01-14 22:25:00 -0300

    feat: criado container spinner

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,36 +1,43 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router';
-import { Card, DefaultLayout } from '../../components';
+import { Card, DefaultLayout, LoadingSpinner } from '../../components';
 import { GetDragons } from '../../store/modules/dragons';
 import { RootState } from '../../store/modules/rootTypes';
 import './styles.scss';
 
 const Home = (): JSX.Element => {
   const navigate = useNavigate();
-  const { dragons } = useSelector((state: RootState) => ({
+  const { dragons, status } = useSelector((state: RootState) => ({
     dragons: state.dragon.dragons,
+    status: state.app.requestStatus,
   }));
 
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(GetDragons());
-  }, []);
+    if (!dragons.length) dispatch(GetDragons());
+  }, [dragons]);
 
   return (
     <DefaultLayout>
       <div className="home">
-        {dragons.map((dragon, key) => (
-          <Card
-            key={key}
-            position={dragon.id}
-            name={dragon.name}
-            canDelete={() => console.log('eerae')}
-            canEdit={() => console.log('edit')}
-            onClick={() => navigate(`/home/${dragon.id}`)}
-          />
-        ))}
+        {status === 'RESOLVE' ? (
+          dragons?.map((dragon, key) => (
+            <Card
+              key={key}
+              position={dragon.id}
+              name={dragon.name}
+              canDelete={() => console.log('eerae')}
+              canEdit={() => console.log('edit')}
+              onClick={() => navigate(`/home/${dragon.id}`)}
+            />
+          ))
+        ) : (
+          <div className="container__spinner">
+            <LoadingSpinner size={200} />
+          </div>
+        )}
       </div>
     </DefaultLayout>
   );
