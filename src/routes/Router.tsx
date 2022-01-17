6cc024b7fb4c2af24938d87913098a81ccbf4db0
Vicente Junior <vicente19981@live.com>
2022-01-17 11:27:08 -0300

    feat: adicionando todas rotas require Auth

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,14 +1,38 @@
 import React from 'react';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import { Detail, Home, Register } from '../pages';
+import Login from '../pages/Login';
+import RequireAuth from './RequireAuth';
 
 const Router = (): JSX.Element => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="home" element={<Home />} />
-        <Route path="home/:dragonId" element={<Detail />} />
-        <Route path="register" element={<Register />} />
+        <Route path="login" element={<Login />} />
+        <Route
+          path="/home"
+          element={
+            <RequireAuth>
+              <Home />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/home/:dragonId"
+          element={
+            <RequireAuth>
+              <Detail />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="register"
+          element={
+            <RequireAuth>
+              <Register />
+            </RequireAuth>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
